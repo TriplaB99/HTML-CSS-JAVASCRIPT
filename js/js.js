5bfde3f2b3d23cf50a9aff743da225bd0e819423
TriplaB99 <66218816+TriplaB99@users.noreply.github.com>
2020-06-06 17:55:40 +0200

    Moonlight

--- js/js.js
+++ js/js.js
@@ -1,5 +1,15 @@
-let video = document.querySelector('video');
+let bg = document.getElementById("bg");
+let moon = document.getElementById("moon");
+let mountain = document.getElementById("mountain");
+let road = document.getElementById("road");
+let text = document.getElementById("text");
+
 window.addEventListener('scroll', function() {
-    let value = window.scrollY;
-    video.style.opacity = 1 + value / -800;
+    var value = window.scrollY;
+
+    bg.style.top = value * 0.5 + 'px';
+    moon.style.left = -value * 0.5 + 'px';
+    mountain.style.top = -value * 0.15 + 'px';
+    road.style.top = value * 0.15 + 'px';
+    text.style.top = value + 'px';
 })
